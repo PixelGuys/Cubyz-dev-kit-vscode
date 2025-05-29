6e10aea778fa22382ddb03aa59026129ffff7e0a
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-05-29 23:22:54 +0200

    Add asset discovery based completion

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,25 +3,23 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 import {
-	createConnection,
-	TextDocuments,
-	Diagnostic,
-	DiagnosticSeverity,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	CompletionItem,
-	CompletionItemKind,
-	TextDocumentPositionParams,
-	TextDocumentSyncKind,
-	InitializeResult,
-	DocumentDiagnosticReportKind,
-	type DocumentDiagnosticReport
-} from 'vscode-languageserver/node';
+    createConnection,
+    TextDocuments,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult,
+    DidChangeWatchedFilesNotification,
+    WatchKind,
+} from "vscode-languageserver/node";
 
-import {
-	TextDocument
-} from 'vscode-languageserver-textdocument';
+import { TextDocument } from "vscode-languageserver-textdocument";
+import * as fs from "fs";
+import * as path from "path";
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -34,61 +32,184 @@ let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
 let hasDiagnosticRelatedInformationCapability = false;
 
+class Asset {
+    id: string;
+    location: string;
+    kind: CompletionItemKind;
+
+    constructor(id: string, location: string, kind: CompletionItemKind) {
+        this.id = id;
+        this.location = location;
+        this.kind = kind;
+    }
+}
+
+class AssetIndex {
+    blocks: Asset[] = [];
+    blockTextures: Asset[] = [];
+    items: Asset[] = [];
+    itemTextures: Asset[] = [];
+    tools: Asset[] = [];
+    biomes: Asset[] = [];
+    models: Asset[] = [];
+    structureBuildingBlocks: Asset[] = [];
+    blueprints: Asset[] = [];
+
+    static async new() {
+        var index = new AssetIndex();
+
+        for (const addon of await fs.promises.readdir("assets/", {
+            withFileTypes: true,
+            recursive: false,
+        })) {
+            if (!addon.isDirectory()) continue;
+
+            await AssetIndex.registerAsset(
+                index.blocks,
+                "blocks",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.items,
+                "items",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.tools,
+                "tools",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.biomes,
+                "biomes",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.models,
+                "models",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.structureBuildingBlocks,
+                "sbb",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+            await AssetIndex.registerAsset(
+                index.blueprints,
+                "sbb",
+                addon.name,
+                ".zon",
+                CompletionItemKind.Class
+            );
+        }
+        return index;
+    }
+
+    static async registerAsset(
+        storage: Asset[],
+        scope: string,
+        addon: string,
+        extension: string,
+        kind: CompletionItemKind
+    ) {
+        const path = `assets/${addon}/${scope}/`;
+        if (!fs.existsSync(path) || !fs.statSync(path).isDirectory()) return;
+
+        for (const file of await fs.promises.readdir(path, {
+            withFileTypes: true,
+            recursive: true,
+        })) {
+            if (!file.isFile()) continue;
+            if (!file.name.endsWith(extension)) continue;
+            if (file.name.startsWith("_default")) continue;
+            if (file.name.startsWith("_migrations")) continue;
+
+            const fileName = file.name.replace(/\.zig\.zon$/, "").replace(/\.zon$/, "");
+            const parentPath = file.parentPath.replace("//", "/").replace("\\", "/");
+            const relativePath = parentPath
+                .replace(/^assets\//, "")
+                .replace(new RegExp("^" + addon + "/"), "")
+                .replace(new RegExp("^" + scope + "/"), "");
+
+            storage.push({
+                id: `${addon}:${relativePath}${fileName}`,
+                location: parentPath + fileName,
+                kind: kind,
+            });
+        }
+    }
+}
+
+var ASSET_INDEX: AssetIndex | null = null;
+
 connection.onInitialize((params: InitializeParams) => {
-	const capabilities = params.capabilities;
+    const capabilities = params.capabilities;
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-	hasWorkspaceFolderCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.workspaceFolders
-	);
-	hasDiagnosticRelatedInformationCapability = !!(
-		capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation
-	);
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
 
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-			// Tell the client that this server supports code completion.
-			completionProvider: {
-				resolveProvider: true
-			},
-			diagnosticProvider: {
-				interFileDependencies: false,
-				workspaceDiagnostics: false
-			}
-		}
-	};
-	if (hasWorkspaceFolderCapability) {
-		result.capabilities.workspace = {
-			workspaceFolders: {
-				supported: true
-			}
-		};
-	}
-	return result;
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            },
+        },
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true,
+            },
+        };
+    }
+    return result;
 });
 
-connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
+connection.onInitialized(async () => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
+        connection.client.register(DidChangeWatchedFilesNotification.type, {
+            watchers: [{ globPattern: "**/*.zon", kind: WatchKind.Change }],
+        });
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+            connection.console.log("Workspace folder change event received.");
+        });
+    }
+    connection.console.log("Initialized Cubyz Dev Kit Language Server");
+    ASSET_INDEX = await AssetIndex.new();
 });
 
 // The example settings
 interface ExampleSettings {
-	maxNumberOfProblems: number;
+    maxNumberOfProblems: number;
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
@@ -100,150 +221,51 @@ let globalSettings: ExampleSettings = defaultSettings;
 // Cache the settings of all open documents
 const documentSettings = new Map<string, Thenable<ExampleSettings>>();
 
-connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = (
-			(change.settings.languageServerExample || defaultSettings)
-		);
-	}
-	// Refresh the diagnostics since the `maxNumberOfProblems` could have changed.
-	// We could optimize things here and re-fetch the setting first can compare it
-	// to the existing setting, but this is out of scope for this example.
-	connection.languages.diagnostics.refresh();
+connection.onDidChangeConfiguration((change) => {
+    if (hasConfigurationCapability) {
+        // Reset all cached document settings
+        documentSettings.clear();
+    } else {
+        globalSettings = change.settings.cubyzDevKit || defaultSettings;
+    }
+    // Refresh the diagnostics since the `maxNumberOfProblems` could have changed.
+    // We could optimize things here and re-fetch the setting first can compare it
+    // to the existing setting, but this is out of scope for this example.
+    connection.languages.diagnostics.refresh();
 });
-
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'languageServerExample'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
-}
 
 // Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
+documents.onDidClose((e) => {
+    documentSettings.delete(e.document.uri);
 });
 
-
-connection.languages.diagnostics.on(async (params) => {
-	const document = documents.get(params.textDocument.uri);
-	if (document !== undefined) {
-		return {
-			kind: DocumentDiagnosticReportKind.Full,
-			items: await validateTextDocument(document)
-		} satisfies DocumentDiagnosticReport;
-	} else {
-		// We don't know the document. We can either try to read it from disk
-		// or we don't report problems for it.
-		return {
-			kind: DocumentDiagnosticReportKind.Full,
-			items: []
-		} satisfies DocumentDiagnosticReport;
-	}
-});
-
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent(change => {
-	validateTextDocument(change.document);
-});
-
-async function validateTextDocument(textDocument: TextDocument): Promise<Diagnostic[]> {
-	// In this simple example we get the settings for every validate run.
-	const settings = await getDocumentSettings(textDocument.uri);
-
-	// The validator creates diagnostics for all uppercase words length 2 and more
-	const text = textDocument.getText();
-	const pattern = /\b[A-Z]{2,}\b/g;
-	let m: RegExpExecArray | null;
-
-	let problems = 0;
-	const diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-		problems++;
-		const diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Spelling matters'
-				},
-				{
-					location: {
-						uri: textDocument.uri,
-						range: Object.assign({}, diagnostic.range)
-					},
-					message: 'Particularly for names'
-				}
-			];
-		}
-		diagnostics.push(diagnostic);
-	}
-	return diagnostics;
-}
-
-connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received a file change event');
+connection.onDidChangeWatchedFiles(async (_change) => {
+    ASSET_INDEX = await AssetIndex.new();
 });
 
 // This handler provides the initial list of the completion items.
-connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		// The pass parameter contains the position of the text document in
-		// which code complete got requested. For the example we ignore this
-		// info and always provide the same completion items.
-		return [
-			{
-				label: 'TypeScript',
-				kind: CompletionItemKind.Text,
-				data: 1
-			},
-			{
-				label: 'JavaScript',
-				kind: CompletionItemKind.Text,
-				data: 2
-			}
-		];
-	}
-);
+connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    var completions: CompletionItem[] = [];
+    if (!!ASSET_INDEX) {
+        ASSET_INDEX.blocks.forEach((element) => {
+            completions.push({
+                label: element.id,
+                kind: CompletionItemKind.Class,
+                data: completions.length,
+            });
+        });
+    }
+    return completions;
+});
 
 // This handler resolves additional information for the item selected in
 // the completion list.
-connection.onCompletionResolve(
-	(item: CompletionItem): CompletionItem => {
-		if (item.data === 1) {
-			item.detail = 'TypeScript details';
-			item.documentation = 'TypeScript documentation';
-		} else if (item.data === 2) {
-			item.detail = 'JavaScript details';
-			item.documentation = 'JavaScript documentation';
-		}
-		return item;
-	}
-);
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    return item;
+});
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
